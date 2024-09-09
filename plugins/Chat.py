7f86091ae0089efca2ed1d4c29b11e770fef00ec
Freeman <freemanfan@qq.com>
2024-09-09 15:46:43 +0800

    为私聊开通了聊天服务（x

--- plugins/Chat.py
+++ plugins/Chat.py
@@ -4,8 +4,7 @@ import json
 import random
 
 
-__plugin_name__ = '聊天'
-
+__plugin_name__ = "聊天"
 
 
 bot = nonebot.get_bot()
@@ -14,25 +13,25 @@ bot = nonebot.get_bot()
 @bot.on_message
 async def _(event: Event):
     raw_message = event.raw_message
-    if f'[CQ:at,qq={event.self_id}]' not in raw_message:
+    if event.message_type == "group" and f"[CQ:at,qq={event.self_id}]" not in raw_message:
         return
-    
+
     chat_reply = Chat(raw_message=raw_message)
     message = chat_reply
-    if chat_reply == None and raw_message.strip() == f'[CQ:at,qq={event.self_id}]':
-        message = '我在。'
+    if chat_reply == None and raw_message.strip() == f"[CQ:at,qq={event.self_id}]":
+        message = "我在。"
     await bot.send(event=event, message=message)
 
 
 def Chat(raw_message: str):
-    with open('./plugins/Config/Chat.json', 'r', encoding='utf-8') as jsonfile:
+    with open("./plugins/Config/Chat.json", "r", encoding="utf-8") as jsonfile:
         data = json.load(jsonfile)
 
     for item in data:
-        order = item.get('order')
+        order = item.get("order")
         for text in order:
             if text in raw_message:
-                reply = item.get('reply')
+                reply = item.get("reply")
                 message = random.choice(reply)
 
                 return message
