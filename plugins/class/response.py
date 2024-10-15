9f0a0a4d0964254cf9c918262a547bb79ce09fcd
Freeman <freemanfan@qq.com>
2024-10-15 21:18:03 +0800

    整理项目结构

--- plugins/class/response.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class FuncResponse():
-    def __init__(self, response_code: int, response_data: str):
-        self.response_code = response_code
-        self.response_data = response_data
-
-
-    
